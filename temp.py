0df115f43c0bc5108346e5ad3597502b6ede7442
nkmin0 <kangmin3374@gmail.com>
2025-03-29 15:30:07 +0900

    temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,8 @@
-print("hello word")
+import serial
+import time
+
+ser = serial.Serial('/dev/serial0', 115200, timeout=1)
+time.sleep(2)
+
+# 출력 포맷을 $VNQMR로 설정 (예시)
+ser.write(b'$VNWRG,01,07*XX\r\n')  # 체크섬 부분은 정확히 계산해서 보내야 함
